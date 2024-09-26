7907ede99736ec0ee12466b9b3b081a80aedc52d
Derek Sivers <derek@sivers.org>
2024-09-26 12:10:10 +1200

    web init

--- web.rb
+++ web.rb
@@ -2,13 +2,17 @@ require 'sinatra'
 require 'pg'
 require 'json'
 
+# SQL query-builder:
+# a('decks') = "select ok, js from srs.decks()"
+# a('review', 9, 'good') = "select ok, js from srs.review($1,$2)", [9, 'good']
+# returns boolean ok, and parsed JSON
 class DBAPI
   def initialize
     @db = PG::Connection.new(dbname: 'srs', user: 'srs')
   end
   def a(func, *params)
     qs = '(%s)' % (1..params.size).map {|i| "$#{i}"}.join(',')
-    sql = "select ok, js from #{func}#{qs}"
+    sql = "select ok, js from srs.#{func}#{qs}"
     r = @db.exec_params(sql, params)[0]
     [
       (r['ok'] == 't'),
@@ -18,8 +22,29 @@ class DBAPI
 end
 API = DBAPI.new
 
-# TODO
 get '/' do
+  ok, @decks = API.a('decks')
   erb :home
+end
+
+post '/' do
+  API.a('add', params[:deck], params[:front], params[:back])
+  redirect to('/')
+end
+
+get '/next' do
+  ok, @card = API.a('next', String(params[:deck]))
+  redirect to('/') unless ok
+  erb :card
+end
+
+post '/card/:id/edit' do
+  API.a('edit', params[:id], params[:deck], params[:front], params[:back])
+  redirect to('/next?deck=%s' % params[:deck])
+end
+
+post '/card/:id/review' do
+  ok, c = API.a('review', params[:id], params[:rating])
+  redirect to('/next?deck=%s' % c[:deck])
 end
 
